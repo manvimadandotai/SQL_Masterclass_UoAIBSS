e89bbd9c11d2854226fe74e8840bb0fb2a3f3c3b
Manvi Madan <42822868+manvimadandotai@users.noreply.github.com>
2021-04-08 11:43:32 +1200

    joins demo queries added

---
+++ Joins.sql
@@ -0,0 +1,35 @@
+SELECT TOP 10 * FROM HumanResources.Employee 
+
+SELECT TOP 10 * FROM Person.[Person]
+
+--INNER JOIN
+--Get employee data from person table for employees in employee table
+SELECT e.*
+FROM HumanResources.Employee as e 
+INNER JOIN Person.Person as p 
+    on e.BusinessEntityID = p.BusinessEntityID
+
+--LEFT OUTER JOIN
+--Only products that have not been ordered 
+SELECT s.SalesOrderID
+       , p.ProductID
+       , p.Name 
+FROM Production.Product as p 
+LEFT OUTER JOIN Sales.SalesOrderDetail as s 
+    ON p.ProductID = s.ProductID
+WHERE SalesOrderID IS NULL 
+
+----An example with with as ----derived tables----
+With orders_2014 as 
+(
+    SELECT *
+    FROM Sales.SalesOrderHeader
+    WHERE  OrderDate BETWEEN '1/1/2014' AND '12/31/2014'
+
+)
+SELECT C.CustomerID
+       , SalesOrderID
+       , OrderDate
+FROM Sales.Customer as C 
+LEFT OUTER JOIN orders_2014 as o 
+    ON C.CustomerID = o.CustomerID
